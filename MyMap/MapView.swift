7a2732a7e665adffafe9bf5a23735f4609e30bf2
chiakiaosima <starsrock5937@gmail.com>
2021-08-01 22:22:30 +0900

    first_commit

---
+++ MyMap/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by 青島智明 on 2021/08/01.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
