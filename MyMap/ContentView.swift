17586cd31425bfc41e3e236d2218e7b1c92957f0
chiakiaosima <starsrock5937@gmail.com>
2021-08-01 22:20:20 +0900

    Initial Commit

---
+++ MyMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMap
+//
+//  Created by 青島智明 on 2021/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 青島智明 on 2021/08/01.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
