a0f0bf98ca700d41e1f595a419dbeb59aed798bf
chiakiaosima <starsrock5937@gmail.com>
2021-08-04 07:29:46 +0900

    マップ種類切り替えボタンの追加

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -36,6 +36,36 @@ struct ContentView: View {
             
             //　マップを表示
                 MapView(searchKey: dispSearchKey, mapType: dispMapType)
+                
+                // マップ種類切り替えボタン
+                Button(action: {
+                    // mapTypeプロパティー値をトグル
+                    //標準　→ 航空写真　→ 航空写真＋標準
+                    //→ 3D Flyover → 3D Flyover+標準
+                    //→ 交通機関
+                    if dispMapType == .standard {
+                        dispMapType = .satellite
+                    } else if dispMapType == .satellite {
+                        dispMapType = .hybrid
+                    } else if dispMapType == .hybrid {
+                        dispMapType = .satelliteFlyover
+                    } else if dispMapType == .satelliteFlyover {
+                        dispMapType =  .hybridFlyover
+                    } else if dispMapType == .hybridFlyover {
+                        dispMapType = .mutedStandard
+                    } else {
+                        dispMapType = .standard
+                    }
+                })  {
+                    //マップアイコン表示
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0, alignment: .leading)
+                }//Buttonここまで
+                //右の余白を20開ける
+                .padding(.trailing, 20.0)
+                //下の余白を30開ける
+                .padding(.bottom, 30.0)
             } //ZStackここまで
         } //VStackここまで
     } //bodyここまで
